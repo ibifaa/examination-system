c79692863454234b35ce2580ea93835b7e1058eb
ibifaa <ibifaaibisakiaruoture@gmail.com>
2026-05-02 08:07:51 +0100

    created database and setup prisma as ORM

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router();
+
+
+
+module.exports = router
+// const express = require('express')
+const app = express();
+
+
+app.listen(4000, ()=>{
+    console.log('Server is running on port 4000')
+})
+
